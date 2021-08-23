ad1623d8354909d7cb8f251af9be3cf62d9cb0b0
billyeatcookies <billydevbusiness@gmail.com>
2021-08-23 05:02:19 -0500

    `hash_object()` command

--- liboreogit/liboreogit.py
+++ liboreogit/liboreogit.py
@@ -29,6 +29,22 @@ argsp.add_argument("object",
 	metavar="object",
 	help="The object to display")
 
+argsp = argsubparsers.add_parser(
+	"hash-object",
+	help="Compute object ID and optionally creates a blob from a file")
+argsp.add_argument("-t",
+	metavar="type",
+	dest="type",
+	choices=["blob", "commit", "tag", "tree"],
+	default="blob",
+	help="Specify a type")
+argsp.add_argument("-w",
+	dest="write",
+	action="store_true",
+	help="Actually write the object into database")
+argsp.add_argument("path",
+	help="Read object from <file>")
+
 class GitRepository(object):
     """
     A git repository
@@ -254,6 +270,34 @@ def cmd_cat_file(args):
 def cat_file(repo, obj, fmt=None):
 	obj = object_read(repo, object_find(repo, obj, fmt=fmt))
 	sys.stdout.buffer.write(obj.serialize())
+
+def cmd_hash_object(args):
+	if args.write:
+		repo = GitRepository(".")
+	else:
+		repo = None
+
+	with open(args.path, "rb") as fd:
+		sha = objec_hash(fd, args.type.encode(), repo)
+		print(sha)
+
+def object_hash(fd, fmt, repo=None):
+	data = fd.read()
+
+	# Choose constructor depending on 
+	# object type found in header
+	if fmt == b'commit':
+		obj = GitCommit(repo, data)
+	elif fmt == b'tree':
+		obj = GitTree(repo, data)
+	elif fmt == b'tag':
+		obj = GitTag(repo, data)
+	elif fmt == b'blob':
+		obj = GitBlob(repo, data)
+	else:
+		raise Exception(f"Unknown type {fmt}")
+
+	return object_write(obj, repo)
 
 def main(argv=sys.argv[1:]):
 	args = argparser.parse_args(argv)
