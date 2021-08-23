43d9f41793c123727891edc8e0b331b4a04b20ec
billyeatcookies <billydevbusiness@gmail.com>
2021-08-23 02:15:24 -0500

    handle command’s argument.

--- liboreogit/liboreogit.py
+++ liboreogit/liboreogit.py
@@ -15,6 +15,13 @@ argsubparsers.required = True
 
 argsp = argsubparsers.add_parser("init", help="Initialize a new, empty repository.")
 
+argsp.add_argument("path",
+	metavar="directory",
+	nargs="?",
+	default=".",
+	help="Where to create the repository.")
+
+
 class GitRepository(object):
     """
     A git repository
