9d19c987e29c37715e6c754445a8277feccaa18a
billyeatcookies <billydevbusiness@gmail.com>
2021-08-23 05:48:02 -0500

    Key-Value List with Message Parsing     - `kvlm_parse()` function     - `kvlm_serialize()` function

--- liboreogit/liboreogit.py
+++ liboreogit/liboreogit.py
@@ -299,6 +299,76 @@ def object_hash(fd, fmt, repo=None):
 
 	return object_write(obj, repo)
 
+# Short for "Key-Value List with Message"
+def kvlm_parse(raw, start=0, dct=None):
+	if not dct:
+		dct = collections.OrderedDict()
+		# declaring the argument as dct=OrderedDict() will make all
+		# functions calls endlessly grow the same dict.
+
+	# search for the next space and the next newline
+	spc = raw.find(b' ', start)
+	nl = raw.find(b'\n', start)
+
+	# If space appears before newline, we have a keyword
+
+	# Base Case
+	# =========
+	# If newline appears first (or there's no space at all, in which
+	# case find returns -1), we assume a blank line. A blank line
+	# means the remainder of the data is the message.
+	if (spc < 0) or (nl < spc):
+		assert(nl == start)
+		dct[b''] = raw[start+1:]
+		return dct
+
+	# Recursive Case
+	# ==============
+	# read a key-value pair and recurse for the next.
+	key = raw[start:spc]
+
+	# Find the end of the value. Continuation lines begin with a
+	# space, so we loop until finds a "\n" not followed by a space.
+	end = start
+	while True:
+		end = raw.find(b'\n', end+1)
+		if raw[end+1] != ord(' '): break
+
+	# Grab the value
+	# Also, drop the leading space on continuation lines
+	value = raw[spc+1:end].replace(b'\n ', b'\n')
+
+	# Don't overwrite existing data contents
+	if key in dct:
+		if type(dct[key]) == list:
+			dct[key].append(value)
+		else:
+			dct[key] = [dct[key], value]
+	else:
+		dct[key] = value
+
+	return kvlm_parse(raw, start=end+1, dct=dct)
+
+def kvlm_serialize(kvlm):
+	ret = b''
+
+	# Output fields
+	for k in kvlm.keys():
+		# Skip the message itself
+		if k == b'': continue
+		val = kvlm[k]
+		# Normalize to a list
+		if type(val) != list:
+			val = [val]
+
+		for v in val:
+			ret += k + b' ' + (v.replace(b'\n', b'\n ')) + b'\n'
+
+	# Append message
+	ret += b'\n' + kvlm[b'']
+
+	return ret
+
 def main(argv=sys.argv[1:]):
 	args = argparser.parse_args(argv)
 
