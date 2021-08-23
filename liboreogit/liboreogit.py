fd369d28e056d1f16ba9704f89770fbe3fb34b60
billyeatcookies <billydevbusiness@gmail.com>
2021-08-23 02:08:28 -0500

    The Repository object implementation and utility functions

--- liboreogit/liboreogit.py
+++ liboreogit/liboreogit.py
@@ -1,4 +1,4 @@
-import argparse
+ import argparse
 import collections
 import configparser
 import hashlib
@@ -13,36 +13,147 @@ argparser = argparse.ArgumentParser(description="This stupid content parser")
 argsubparsers = argparser.add_subparsers(title="Commands", dest="command")
 argsubparsers.required = True
 
+class GitRepository(object):
+    """
+    A git repository
+    """
+
+    worktree = None
+    gitdir = None
+    conf = None
+
+    def __init__(self, path, force=False):
+        self.worktree = path
+        self.gitdir = os.path.join(path, ".git")
+
+        if not (force or os.path.isdir(self.gitdir)):
+            raise Exception(f"Not a Git repository {path}")
+
+        # Read configuration file in .git/config
+        self.conf = configparser.ConfigParser()
+        cf = repo_file(self, "config")
+
+        if cf and os.path.exists(cf):
+            self.conf.read([cf])
+        elif not force:
+            raise Exception("Configuration file missing")
+
+        if not force:
+            vers = int(self.conf.get("core", "repositoryformatversion"))
+            if vers != 0:
+                raise Exception(f"Unsupported repositoryformatversion {vers}")
+
+def repo_path(repo, *path):
+	"""
+	Compute path under repo's gitdir
+	"""
+	return os.path.join(repo.gitdir, *path)
+
+def repo_file(repo, *path, mkdir=False):
+	"""
+	Same as repo_path, but create dirname(*path) if absent.
+	For example, repo_file(r, 'refs', 'remotes', 'origin', 'HEAD')
+	will create .git/refs/remotes/origin.
+	"""
+
+	if repo_dir(rep, *path[:-1], mkdir=mkdir):
+		return repo_path(repo, *path)
+
+def repo_dir(repo, *path, mkdir=False):
+	"""
+	Same as repo_path, but mkdir *path if absent
+	"""
+
+	path = repo_path(repo, *path)
+
+	if os.path.exists(path):
+		if (os.path.isdir(path)):
+			return True
+		else:
+			raise Exception(f"Not a directory {path}")
+
+	if mkdir:
+		os.makedirs(path)
+		return repo_path
+	else:
+		return 0
+
+
+def repo_create(path):
+	"""Create a new repository at path"""
+
+	repo = GitRepository(path, True)
+
+	# check: path either doesn't exist or is empty
+	if os.path.exists(repo.worktree):
+		if not os.path.isdir(repo.worktree):
+			raise Exception(f"{path} is not a directory!")
+		if os.listdir(repo.worktree):
+			raise Exception(f"{path} is not empty!")
+	else:
+		os.makedirs(repo.worktree)
+
+	assert(repo_dir(repo, "branches", mkdir=True))
+	assert(repo_dir(repo, "objects", mkdir=True))
+	assert(repo_dir(repo, "refs", "tags", mkdir=True))
+	assert(repo_dir(repo, "refs", "heads", mkdir=True))
+
+	# .git/description
+	with open(repo_file(repo, "description"), "w") as f:
+		f.write("Unnamed repository; edit this file 'description' to name the repository.\n")
+
+	# .git/HEAD
+	with open(repo_file(repo, "HEAD"), "w") as f:
+		f.write("ref: refs/heads/master\n")
+
+	with open(repo_file(repo, "config"), "w") as f:
+		config = repo_default_config()
+		config.write(f)
+
+	return repo
+
+
+def repo_default_config():
+	ret = configparser.ConfigParser()
+
+	ret.add_section("core")
+	ret.set("core", "repositoryformatversion", "0")
+	ret.set("core", "filemode", "false")
+	ret.set("core", "bare", "false")
+
+	return ret
+
+
 def main(argv=sys.argv[1:]):
 	args = argparser.parse_args(argv)
 
 	if args.command == "add":
-		git_add(args)
+		cmd_add(args)
 	elif args.command == "cat-file":
-		git_cat_file(args)
+		cmd_cat_file(args)
 	elif args.command == "checkout":
-		git_checkout(args)
+		cmd_checkout(args)
 	elif args.command == "commit":
-		git_commit(args)
+		cmd_commit(args)
 	elif args.command == "hash-object":
-		git_hash_object(args)
+		cmd_hash_object(args)
 	elif args.command == "init":
-		git_init(args)
+		cmd_init(args)
 	elif args.command == "log":
-		git_log(args)
+		cmd_log(args)
 	elif args.command == "ls-tree":
-		git_ls_tree(args)
+		cmd_ls_tree(args)
 	elif args.command == "merge":
-		git_merge(args)
+		cmd_merge(args)
 	elif args.command == "rebase":
-		git_rebase(args)
+		cmd_rebase(args)
 	elif args.command == "rev-parse":
-		git_rev_parse(args)
+		cmd_rev_parse(args)
 	elif args.command == "rm":
-		git_rm(args)
+		cmd_rm(args)
 	elif args.command == "show-ref":
-		git_show_ref()
+		cmd_show_ref()
 	elif args.command == "tag":
-		git_tag()
+		cmd_tag()
 	else:
 		print("no")
