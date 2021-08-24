94fb2cd0135d4933ae16c27e40852d9ef335b14e
billyeatcookies <billydevbusiness@gmail.com>
2021-08-24 07:07:53 -0500

    Parsing Trees - `GitTreeLeaf` object - `tree_parse_one()` function - `tree_parse()` function - `tree_serialize()` function - `GitTree` git object

--- liboreogit/liboreogit.py
+++ liboreogit/liboreogit.py
@@ -417,6 +417,59 @@ def log_graphviz(repo, sha, seen):
         print("c_{0} -> c_{1};".format(sha, p))
         log_graphviz(repo, p, seen)
 
+class GitTreeLeaf(object):
+	def __init__(self, mode, path, sha):
+		self.mode = mode
+		self.path = path
+		self.sha = sha
+
+def tree_parse_one(raw, start=0):
+	# Find the space terminator of the mode
+	x = raw.find(b' ', start)
+	assert(x - start == 5 or x - start == 9)
+
+	# Read the mode
+	mode = raw[start:x]
+
+	# Find the NULL terminator of the path and read the path
+	y = raw.find(b'\x00', x)
+	path = raw[x+1:y]
+
+	# Read the SHA and convert to an hex string
+	sha = hex(
+		int.from_bytes(
+			raw[y+1:y+21], "big"))[2:] # hex() adds 0x in front
+
+	return y + 21, GitTreeLeaf(mode, path, sha)
+
+def tree_parse(raw):
+	pos = 0
+	max = len(raw)
+	ret = list()
+	while pos < max:
+		pos, data = tree_parse_one(raw, pos)
+		ret.append(data)
+
+	return ret
+
+def tree_serialize(obj):
+	ret = b''
+	for i in obj.items:
+		ret += i.mode
+		ret += b' '
+		ret += i.path
+		ret += b'\x00'
+		sha = int(i.sha, 16)
+		ret += sha.to_bytes(20, byteorder="big")
+
+class GitTree(GitObject):
+	fmt = b'tree'
+
+	def deserialze(self, data):
+		self.items = tree_parse(data)
+
+	def serialize(self):
+		return tree_serialize(self)
 
 def main(argv=sys.argv[1:]):
 	args = argparser.parse_args(argv)
