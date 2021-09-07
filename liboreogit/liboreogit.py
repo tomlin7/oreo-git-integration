41809ff4a2532ff61a690931aa3f1a4a3e11b542
billyeatcookies <billydevbusiness@gmail.com>
2021-09-07 04:13:11 -0500

    Parsing the index
    # Feature Additions
    - `GitIndexEntry` class

--- liboreogit/liboreogit.py
+++ liboreogit/liboreogit.py
@@ -695,6 +695,45 @@ def cmd_rev_parse(args):
 	print(object_find(repo, args.name, args.type, follow=True))
 
 
+class GitIndexEntry(object):
+	ctime = None
+	"""The last time a file's metadata changed. THis is a tuple (seconds, nanoseconds)"""
+
+	mtime = None
+	"""The last time a file's data changed. This is a tuple (seconds, nanoseconds)"""
+
+	dev = None
+	"""The ID of the device containing this file"""
+
+	ino = None
+	"""The file's inode number"""
+
+	node_type = None
+	"""The object type, either b1000 (regular), b1010 (symlink), or b1110 (gitlink)."""
+
+	mode_perms = None
+	"""The object permissions, an integer"""
+
+	uid = None
+	"""User ID of owner"""
+
+	gid = None
+	"""Group ID of owner"""
+
+	size = None
+	"""Size of this object, in bytes"""
+
+	obj = None
+	"""The object's hash as a hex string"""
+
+	flag_assume_valid = None
+	flag_extended = None
+	flag_stage = None
+	flag_name_length = None
+	"""Length of the name if < 0xFFF, -1 otherwise"""
+
+	name = None
+
 def main(argv=sys.argv[1:]):
 	args = argparser.parse_args(argv)
 
